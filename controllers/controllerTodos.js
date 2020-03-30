1e85b22d6db787a70b24b3418b6fec78d2e1d84d
mohhaidir <khoiruljmbr@gmail.com>
2020-03-30 14:33:43 +0700

    Mohammad Haidir Put Todos By ID

--- controllers/controllerTodos.js
+++ controllers/controllerTodos.js
@@ -36,7 +36,7 @@ class ControllerTodos {
   }
 
   static getTodosId(req, res) {
-    const pk = req.params.id
+    const pk = Number(req.params.id)
     Todos.findByPk(pk)
       .then(result => {
         if (result == null) {
@@ -54,7 +54,29 @@ class ControllerTodos {
   }
 
   static putTodosId(req, res) {
-
+    const id = Number(req.params.id)
+    const data = req.body
+    // console.log(id)
+    Todos.update(data, { where: { id: id } })
+      .then(result => {
+        if (result[0] == 0) {
+          const msg = 'not found!'
+          res.status(404).json(msg)
+        } else {
+          res.status(200).json(data)
+        }
+      })
+      .catch(err => {
+        if (err) {
+          const errorArgument = []
+          for (let i = 0; i < err.errors.length; i++) {
+            errorArgument.push(err.errors[i].message)
+          }
+          res.status(400).json(errorArgument)
+        } else {
+          res.status(500).json(err)
+        }
+      })
   }
 
   static deleteTodosId(req, res) {
