f402e59c8ad3f3dd166a1ce852eb33f52a7de87c
mohhaidir <khoiruljmbr@gmail.com>
2020-03-30 14:45:04 +0700

    Mohammad Haidir Deleted Todos By ID

--- routers/routerTodos.js
+++ routers/routerTodos.js
@@ -6,6 +6,6 @@ router.post('/', ControllerTodos.postTodos)
 router.get('/', ControllerTodos.getTodos)
 router.get('/edit/:id', ControllerTodos.getTodosId)
 router.put('/edit/:id', ControllerTodos.putTodosId)
-// router.delete('/delete/:id', ControllerTodos.deleteTodosId)
+router.delete('/delete/:id', ControllerTodos.deleteTodosId)
 
 module.exports = router
