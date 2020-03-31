0c03c26327abb5bd3cf60bd9d6d125d5bdd89731
mohhaidir <khoiruljmbr@gmail.com>
2020-03-31 15:11:41 +0700

    Mohammad Haidir Dokumentasi asosiasi, bcrypt, generate TOKEN

--- models/users.js
+++ models/users.js
@@ -1,14 +1,29 @@
 'use strict';
+const hassPassword = require('../helpers/bcrypt')
+
 module.exports = (sequelize, DataTypes) => {
   const Model = sequelize.Sequelize.Model
 
   class Users extends Model { }
 
   Users.init({
-    username: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING
-  }, { sequelize });
+    username: {
+      type: DataTypes.STRING
+    },
+    email: {
+      type: DataTypes.STRING
+    },
+    password: {
+      type: DataTypes.STRING
+    }
+  }, {
+    hooks: {
+      beforeCreate: (instance, options) => {
+        instance.password = hassPassword(instance.password)
+        console.log(instance.password)
+      }
+    }, sequelize
+  });
 
   // const Users = sequelize.define('Users', {
   // })
