3ffe0079410d2322ae43f5d207f1a84ba4603fe5
mohhaidir <khoiruljmbr@gmail.com>
2020-04-01 20:00:02 +0700

    Mohammad Haidir baru login sama register

--- server/app.js
+++ server/app.js
@@ -4,7 +4,9 @@ const port = 3000
 const router = require('./routes')
 const errorHandler = require('./middlewares/errorHandler')
 const axios = require('axios')
+const cors = require('cors')
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
